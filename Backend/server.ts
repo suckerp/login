eb90758f2b09dd1f63496cea128278a5926af17b
suckerp <suckerp@live.de>
2018-12-03 22:41:11 +0100

    start

---
+++ Backend/server.ts
@@ -0,0 +1,24 @@
+import express = require('express')
+import cors = require('cors')
+
+const app = express()
+
+app.use(
+    cors(),
+    express.json(),
+    express.urlencoded()
+)
+
+app.post('/login', (req, res, next) => {
+    console.log(req.headers)
+    res.json("TOKEN")
+})
+
+const port = 3000
+
+app.listen(port, ()=>{
+    console.log(`
+    Server wurde gestartet
+    url: http://localhost:${port} 
+`)
+})
